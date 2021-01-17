ec78f4b63ce3a0f5d96cb292790810abb461e4ca
brians-123 <brian_shor@hotmail.com>
2021-01-17 11:32:01 -0800

    did a little cleanup of console.logs, and other unnecessary items. Created base readme.

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,12 +1,10 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-//id, burger_name, devoured
+//columns in db are: id, burger_name, devoured
 $(function () {
   $(".devour").on("click", function (event) {
-    var id = $(this).data("id");
-    var isDevoured = true;
-
-    var newDevouredState = {
-      devoured: isDevoured,
+    const id = $(this).data("id");
+    let newDevouredState = {
+      devoured: true,
     };
 
     // Send the PUT request.
@@ -14,17 +12,16 @@ $(function () {
       type: "PUT",
       data: newDevouredState,
     }).then(function () {
-      console.log("changed devoured to", isDevoured);
       // Reload the page to get the updated list
       location.reload();
     });
   });
 
+  //jquery to create on click delete
   $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
-    var newBurger = {
+    const newBurger = {
       burger_name: $("#new-burger").val().trim(),
       devoured: false,
     };
@@ -34,20 +31,18 @@ $(function () {
       type: "POST",
       data: newBurger,
     }).then(function () {
-      console.log("created new burger");
       // Reload the page to get the updated list
       location.reload();
     });
   });
 
+  //jquery to create on click delete
   $(".delete-burger").on("click", function (event) {
-    var id = $(this).data("id");
-    console.log("all the things");
+    const id = $(this).data("id");
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE",
     }).then(function () {
-      console.log("deleted burger", id);
       // Reload the page to get the updated list
       location.reload();
     });
